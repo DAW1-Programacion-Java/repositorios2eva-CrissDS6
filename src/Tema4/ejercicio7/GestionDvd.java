ef60ee9762fc3d128d188e0a5ef685dd2b453579
CrissDS6 <cdelgados18@educarex.es>
2025-01-15 19:02:48 +0100

    prueba2

--- src/Tema4/ejercicio7/GestionDvd.java
+++ src/Tema4/ejercicio7/GestionDvd.java
@@ -3,5 +3,6 @@ package Tema4.ejercicio7;
 public class GestionDvd {
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("adios");
     }
 }
